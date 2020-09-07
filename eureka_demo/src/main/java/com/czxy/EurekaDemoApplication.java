cbe9278d40d00ad2b595dce710a5dd2f47aa9ffa
lg1118 <q540609890@163.com>
2020-09-07 13:40:11 +0800

    修改了注释

--- eureka_demo/src/main/java/com/czxy/EurekaDemoApplication.java
+++ eureka_demo/src/main/java/com/czxy/EurekaDemoApplication.java
@@ -3,7 +3,7 @@ package com.czxy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
+//123
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaDemoApplication {
